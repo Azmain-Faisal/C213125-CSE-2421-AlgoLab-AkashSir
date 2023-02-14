8bc91be9fad495b703065847c426afc06fc0098d
Azmain-Faisal <110972909+Azmain-Faisal@users.noreply.github.com>
2023-02-15 05:08:56 +0600

    Update TabulationCode.cpp

--- TabulationCode.cpp
+++ TabulationCode.cpp
@@ -7,13 +7,12 @@ int knapSack(int x, int wt[], int val[], int n)
     int i, j;
     vector<vector<int>> K(n + 1, vector<int>(x + 1));
 
-    for (i = 0; i <= n; i++){
-        for (j = 0; j <= x; j++){
-            if (i == 0 || j == 0){
+    for(i=0;i<=n;i++){
+        for(j =0;j<=x;j++){
+            if(i==0 || j==0){
                 K[i][j] = 0;
-            }else if (wt[i - 1] <= j){
-                K[i][j] = max(val[i - 1] + K[i - 1][j - wt[i - 1]],
-                              K[i - 1][j]);
+            }else if(wt[i - 1]<=j){
+                K[i][j] = max(val[i - 1] + K[i - 1][j - wt[i - 1]],K[i - 1][j]);
             }else{
                 K[i][j] = K[i - 1][j];
             }
@@ -27,9 +26,9 @@ int main()
 {
     int val[mx], wt[mx], n;
     cin >> n;
-    for (int i = 0; i < n; i++){
+    for(int i=0;i<n;i++){
         cin >> val[i];
-    }for (int i = 0; i < n; i++){
+    }for(int i=0;i<n;i++){
         cin >> wt[i];
     }
 
